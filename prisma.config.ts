79bd0526eec20775d0c4936750f88ddc0c3687ae
Alimehrani <radmermehrani@gmail.com>
2026-06-27 10:53:40 -0700

    feat: add Onboarding Wizard, update prisma.config with directUrl

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,9 +5,9 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: "ts-node --compiler-options {\"module\":\"CommonJS\"} prisma/seed.ts",
   },
   datasource: {
     url: process.env.DATABASE_URL!,
+    directUrl: process.env.DIRECT_URL!,
   },
 });
